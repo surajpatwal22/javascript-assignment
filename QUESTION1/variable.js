c0e6173b524e4f42b6024ec4eb38451381dd8a4e
surajpatwal22 <patwal0suraj@gmail.com>
2023-02-12 00:10:55 -0800

    first2question

---
+++ QUESTION1/variable.js
@@ -0,0 +1,16 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+// declaring variable through example
+
+// for string 
+ var name = " suraj patwal ";
+
+//  for null 
+let name = null ;
+
+// for Boolean
+
+let x = " true";
+if (x){
+    console.log("lets party");
+}
---
+++ QUESTION2/question2.js
@@ -0,0 +1,9 @@
+// Declare variables to store your first name, last name, marital status, country and age and display them using interploation method
+
+var firstname = "suraj";
+var lastname = "Patwal";
+var maritalStatus = "single"
+var country = " india";
+var age = 26;
+
+console.log(`name of candidate is ${firstname} ${lastname} and he is ${maritalStatus} around ${age} year old from ${country}`);
